c93be7883b2fc73b0f06209a5a1e1b9d703d1677
Dalto Curvelano <dalto@toptal.com>
2017-01-03 17:18:43 +0300

    это отстой

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -1,36 +1,35 @@
 package tasks
 
 import (
-	"encoding/json"
 	"errors"
-	 "fmt"
-	"github.com/HouzuoGuo/tiedot/db"
+	_ "fmt"
 	"github.com/deckarep/gosx-notifier"
 	"github.com/fatih/color"
 	"github.com/olekukonko/tablewriter"
+ 	"github.com/asdine/storm"
 	"os"
 	"strconv"
-	"strings"
+	_ "strings"
 	"time"
 )
 
-const myDBDir = "/Users/dlt/.selfcontrol"
-
 var (
 	errDuplicateTimerForTask = errors.New("task already has a running timer")
-	tasksCollection          *db.Col
-	timersCollection         *db.Col
-	DB                       *db.DB
-	ticker 			 chan time.Time
+	ticker 			 <-chan time.Time
+	DB			 *storm.DB
 )
 
-type task map[string]interface{}
+type task struct {
+	ID int `storm:"id,increment"`
+	Name string `storm:"unique"`
+	Status string
+}
 
 type taskTimer struct {
+	ID         int `storm:"id,increment"`
 	TaskID     int
-	Timer      *time.Timer
 	StartedAt  time.Time
-	ExpiresAt time.Time
+	ExpiresAt  time.Time
 	Message    string
 	Fired      bool
 }
@@ -38,7 +37,7 @@ type taskTimer struct {
 func (tt *taskTimer) trigger() {
 	pushNotification(tt.Message)
 	tt.Fired = true
-	tt.save()
+	tt.update()
 }
 
 func (tt *taskTimer) ellapsedTime() time.Duration {
@@ -55,73 +54,37 @@ func (timer *taskTimer) unfinished() bool {
 	return !timer.Fired
 }
 
-func (timer *taskTimer) save() {
-	timersDoc := map[string]interface{}{
-		"TaskID":     timer.TaskID,
-		"Fired":      timer.Fired,
-		"Message":    timer.Message,
-		"StartedAt":  timer.StartedAt,
-		"ExpiresAt":  timer.ExpiresAt,
-	}
-	_, err := timersCollection.Insert(timersDoc)
+func (timer *taskTimer) update() {
+	err := DB.Update(&timer)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func init() {
-	d, err := db.OpenDB(myDBDir)
+	d, err := storm.Open("selfcontrol.db")
 	DB = d
 	if err != nil {
 		panic(err)
 	}
-	DB.Create("Tasks")
-	DB.Create("Timers")
-	tasksCollection = DB.Use("Tasks")
-	timersCollection = DB.Use("Timers")
 	startTimersLoop()
 }
 
 func startTimersLoop()  {
     ticker = time.Tick(1 * time.Second)
-    for now := range ticker {
-	    fmt.Println("it works")
-    }
-}
-
-func loadTimers() {
-	timers = make(map[int][]*taskTimer)
-	timersCollection.ForEachDoc(func(id int, raw []byte) (willMoveOn bool) {
-		var doc map[string]interface{}
-		json.Unmarshal(raw, &doc)
-		startedAt, err := time.Parse(time.RFC3339, doc["StartedAt"].(string))
-		if err != nil {
-			panic(err)
-		}
-		expiresAt, err := time.Parse(time.RFC3339, doc["ExpiresAt"].(string))
-		if err != nil {
-			panic(err)
-		}
-		taskID := int(doc["TaskID"].(float64))
-		timer := &taskTimer{
-			TaskID:     taskID,
-			Fired:      doc["Fired"].(bool),
-			StartedAt:  startedAt,
-			ExpiresAt:  expiresAt,
-			Persisted:  true,
-		}
-		timers[shortID(taskID)] = append(timers[shortID(taskID)], timer)
-		return true
-	})
+    go func ()  {
+	    for _ = range ticker {
+	    }
+    }()
 }
 
 // Create a new task given its name
 func Create(name string) {
-	task := map[string]interface{}{
-		"name":   name,
-		"status": "TODO",
+	timer := task{
+		Name: name,
+		Status: "TODO",
 	}
-	_, err := tasksCollection.Insert(task)
+	err := DB.Save(&timer)
 	if err != nil {
 		panic(err)
 	}
@@ -139,124 +102,104 @@ func Print() {
 }
 
 // Delete a task with given id
-func Delete(id int) {
-	if err := tasksCollection.Delete(id); err != nil {
+func Delete(ID int) {
+	var t task
+	err := DB.Find("ID", ID, &t)
+	if err != nil {
+		panic(err)
+	}
+	if err = DB.DeleteStruct(&t); err != nil {
 		panic(err)
 	}
 }
 
 // UpdateFields updates the tasks attributes
-func UpdateFields(taskID int, fieldValuePairs []string) (bool, error) {
-	task, err := tasksCollection.Read(taskID)
-	if err != nil {
-		return false, err
-	}
-	for _, pair := range fieldValuePairs {
-		arr := strings.Split(pair, ":")
-		field, value := arr[0], arr[1]
-		if field == "status" {
-			value = strings.ToUpper(value)
-		}
-		task[field] = value
-	}
-	if err = tasksCollection.Update(taskID, task); err != nil {
-		return false, err
-	}
-	return true, nil
-}
+// func UpdateFields(taskID int, fieldValuePairs []string) (bool, error) {
+// 	task, err := tasksCollection.Read(taskID)
+// 	if err != nil {
+// 		return false, err
+// 	}
+// 	for _, pair := range fieldValuePairs {
+// 		arr := strings.Split(pair, ":")
+// 		field, value := arr[0], arr[1]
+// 		if field == "status" {
+// 			value = strings.ToUpper(value)
+// 		}
+// 		task[field] = value
+// 	}
+// 	if err = tasksCollection.Update(taskID, task); err != nil {
+// 		return false, err
+// 	}
+// 	return true, nil
+// }
 
 // AddTimerForTask adds a timer for a given task id and duration
 func AddTimerForTask(taskID int, d time.Duration) (bool, error) {
-	task, err := tasksCollection.Read(taskID)
+	var t task
+	err := DB.Find("ID", taskID, &t)
 	if err != nil {
 		panic(err)
 	}
 
-	if hasRunningTimer(taskID) {
-		return false, errDuplicateTimerForTask
-	}
+//	if hasRunningTimer(taskID) {
+//		return false, errDuplicateTimerForTask
+//	}
 
-	name := task["name"].(string)
-	timer := time.NewTimer(d)
-	message := "Timer for '" + name + "' finished!"
-	tTimer := &taskTimer{
+	name := t.Name
+	startedAt := time.Now()
+	expiresAt := startedAt.Add(d)
+
+	DB.Save(&taskTimer{
 		TaskID:     taskID,
-		Timer:      timer,
-		Message:    message,
-		StartedAt:  time.Now(),
-		ExpiresAt: time.Time{},
-	}
-
-	tid := shortID(taskID)
-	if timers[tid] == nil {
-		timers[tid] = make([]*taskTimer, 0)
-	}
-
-	timers[tid] = append(timers[tid], tTimer)
-
-	go func() {
-		<-timer.C
-		tTimer.trigger()
-		Print()
-	}()
+		Message:    "Timer for '" + name + "' finished!",
+		StartedAt:  startedAt,
+		ExpiresAt:  expiresAt,
+	})
 	return true, nil
 }
 
-// Save saves unpersisted timers upon console exit
-func Save() {
-	for _, taskTimers := range timers {
-		for _, timer := range taskTimers {
-			if !timer.Persisted {
-				timer.save()
-			}
-		}
-	}
-}
-
-func hasRunningTimer(taskID int) bool {
-	for _, timer := range timers[taskID] {
-		if timer.unfinished() {
-			return true
-		}
-	}
-	return false
-}
+// func hasRunningTimer(taskID int) bool {
+// 	for _, timer := range timers[taskID] {
+// 		if timer.unfinished() {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
 
 func pushNotification(message string) {
 	gosxnotifier.NewNotification(message).Push()
 }
 
 func createRows() [][]string {
-	loadTimers()
-
 	var rows = [][]string{}
-	tasksCollection.ForEachDoc(func(id int, raw []byte) (willMoveOn bool) {
-		var doc task
-		json.Unmarshal(raw, &doc)
-		name := doc["name"].(string)
-		status := doc["status"].(string)
+	var tasks []task
+	err := DB.All(&tasks)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, tt := range tasks {
 		row := []string{
-			strconv.Itoa(id),
-			name,
-			coloredStatus(status),
-			totalRunningTime(id).String(),
+			strconv.Itoa(tt.ID),
+			tt.Name,
+			coloredStatus(tt.Status),
+			totalRunningTime(tt.ID).String(),
 		}
 		rows = append(rows, row)
-		return true
-	})
+	}
 	return rows
-}
-
-func shortID(id int) int {
-	str := strconv.Itoa(id)
-	i, _ := strconv.Atoi(str[0:4])
-	return i
 }
 
 func totalRunningTime(taskID int) time.Duration {
 	total := time.Duration(0)
-	for _, timer := range timers[shortID(taskID)] {
-		total += timer.ellapsedTime()
+	var taskTimers []taskTimer
+	err := DB.Find("ID", taskID, &taskTimers)
+	if err != nil {
+		panic(err)
+	}
+	for _, t := range taskTimers {
+		total += t.ellapsedTime()
 	}
 	return total
 }
