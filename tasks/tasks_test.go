924c1f3fc3b674370127cc451c0039f6f410212b
Dalto Curvelano <dalto@toptal.com>
2017-01-11 16:24:17 +0100

    Add test for Delete func

--- tasks/tasks_test.go
+++ tasks/tasks_test.go
@@ -1,49 +1,66 @@
 package tasks
 
 import (
-        "testing"
-        "path/filepath"
-        "os"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"path/filepath"
+	"testing"
 )
 
 var (
-        testdbfile string = filepath.Join(os.Getenv("HOME"), ".selfcontrol_test.db")
+	testdbfile string = filepath.Join(os.Getenv("HOME"), ".selfcontrol_test.db")
 )
 
 func TestMain(m *testing.M) {
-        Init(testdbfile)
-        m.Run()
-        resetDB()
+	Init(testdbfile)
+	m.Run()
+	resetDB()
 }
 
-func resetDB()  {
-        err := os.Remove(testdbfile)
-        if  err != nil {
-                panic(err)
-        }
+func resetDB() {
+	err := os.Remove(testdbfile)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func TestAdd(t *testing.T)  {
-        Add("foo", make([]string, 0))
-        var tsk task
-        err := DB.One("Name", "foo", &tsk)
-        if err != nil {
-                t.Error(err)
-        }
-        if tsk.Name != "foo" {
-                t.Error("expected task name", "foo", tsk.Name)
-        }
+func TestAdd(t *testing.T) {
+	Add("foo", make([]string, 0))
+	var tsk task
+	err := DB.One("Name", "foo", &tsk)
+	if err != nil {
+		t.Error(err)
+	}
+	if tsk.Name != "foo" {
+		t.Error("expected task name", "foo", tsk.Name)
+	}
 
-        fieldValuePairs := []string{"pri:1", "status:done"}
-        Add("bar", fieldValuePairs)
-        err = DB.One("Name", "bar", &tsk)
-        if err != nil {
-                t.Error(err)
-        }
-        if tsk.Priority != 1 {
-                t.Error("expected task priority", 1, tsk.Priority)
-        }
-        if tsk.Status != "DONE" {
-                t.Error("expected task status", "done", tsk.Status)
-        }
+	fieldValuePairs := []string{"pri:1", "status:done"}
+	Add("bar", fieldValuePairs)
+	err = DB.One("Name", "bar", &tsk)
+	if err != nil {
+		t.Error(err)
+	}
+	if tsk.Priority != 1 {
+		t.Error("expected task priority", 1, tsk.Priority)
+	}
+	if tsk.Status != "DONE" {
+		t.Error("expected task status", "done", tsk.Status)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	Add("foo", make([]string, 0))
+	var tsk task
+	err := DB.One("Name", "foo", &tsk)
+	if err != nil {
+		t.Error(err)
+	}
+	if tsk.Name != "foo" {
+		t.Error("expected task name", "foo", tsk.Name)
+	}
+
+	Delete(tsk.ID)
+	err = DB.One("Name", "foo", &tsk)
+	assert.Error(t, err, "not found error was expected")
 }
